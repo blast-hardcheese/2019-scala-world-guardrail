672ef9e18c02397e98692dd5b4ef22b425ee5125
Devon Stewart <dstewart@twilio.com>
2019-09-04 07:03:55 -0700

    updating

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -5,7 +5,19 @@ import scala.concurrent.Future
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server._
 import akka.http.scaladsl._
+import swagger.definitions._
+import swagger.examples.user.{ UserHandler, UserResource }
 
 object Foo extends App {
-  
+  new UserHandler {
+    def createUser(respond: UserResource.createUserResponse.type
+      )(body: Option[User]
+      ): Future[UserResource.createUserResponse] = {
+        Future.successful(respond.Created)
+    }
+
+    def getUser(respond: UserResource.getUserResponse.type
+      )(id: Long
+      ): Future[UserResource.getUserResponse] = ???
+  }
 }
