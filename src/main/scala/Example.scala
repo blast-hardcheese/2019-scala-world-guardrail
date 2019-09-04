6f7e11ff28a52976be35560b9faa7b7c6dbba476
Devon Stewart <dstewart@twilio.com>
2019-09-04 07:24:26 -0700

    updating

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -1,12 +1,15 @@
 package se.hardchee
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server._
 import akka.http.scaladsl._
+import cats.data._
+import cats.implicits._
 import swagger.definitions._
-import swagger.examples.user.{ UserHandler, UserResource }
+import swagger.examples.user.{ UserClient, UserHandler, UserResource }
 
 object Foo extends App {
   implicit val as = akka.actor.ActorSystem()
@@ -27,9 +30,30 @@ object Foo extends App {
   val route = UserResource.routes(handler)
   val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
-  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-  scala.io.StdIn.readLine() // let it run until user presses return
-  bindingFuture
-    .flatMap(_.unbind()) // trigger unbinding from the port
-    .onComplete(_ => as.terminate()) // and shutdown when done
+	val client = UserClient.httpClient(Route.asyncHandler(route))
+
+  case class ErrorModel(message: String)
+
+  val handleAkkaCrap: Either[Throwable, HttpResponse] => ErrorModel =
+    _.fold(t => ErrorModel(t.toString()), r => ErrorModel(r.toString()))
+
+  val x = for {
+    userResp <- client.createUser().leftMap(handleAkkaCrap)
+  } yield {
+    userResp.fold(
+      handleOK=_.fullName,
+      handleForbidden="Couldn't get the user's name"
+    )
+  }
+
+  val y = Await.result(x.value, 60.seconds)
+  println(y)
+
+  as.terminate()
+
+//  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+//  scala.io.StdIn.readLine() // let it run until user presses return
+//  bindingFuture
+//    .flatMap(_.unbind()) // trigger unbinding from the port
+//    .onComplete(_ => as.terminate()) // and shutdown when done
 }
