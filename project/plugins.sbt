5b1a52c13f92fe9646140145619282bef84cefc4
Devon Stewart <dstewart@twilio.com>
2019-09-04 07:10:34 -0700

    updating

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,3 @@
 addSbtPlugin("com.twilio" % "sbt-guardrail" % "0.53.1")
+
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.9.1")
--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -9,15 +9,27 @@ import swagger.definitions._
 import swagger.examples.user.{ UserHandler, UserResource }
 
 object Foo extends App {
-  new UserHandler {
+  implicit val as = akka.actor.ActorSystem()
+  implicit val mat = akka.stream.ActorMaterializer()
+
+  val handler = new UserHandler {
     def createUser(respond: UserResource.createUserResponse.type
       )(body: Option[User]
       ): Future[UserResource.createUserResponse] = {
-        Future.successful(respond.OK)
+        Future.successful(body.fold(respond.Forbidden)(respond.OK))
     }
 
     def getUser(respond: UserResource.getUserResponse.type
       )(id: Long
       ): Future[UserResource.getUserResponse] = ???
   }
+
+  val route = UserResource.routes(handler)
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  scala.io.StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => as.terminate()) // and shutdown when done
 }
